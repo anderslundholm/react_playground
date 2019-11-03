502c29c1e4dbcf0d19dfe1d00882e7c92e8884f3
Anders Lundholm <flaskhalz@gmail.com>
2019-11-03 11:07:41 +0100

    Added bootstrap and react-bootstrap.

--- react-test/src/components/App/App.js
+++ react-test/src/components/App/App.js
@@ -4,7 +4,7 @@ import Header from '../Header/Header'
 import Home from '../routes/Home'
 import About from '../routes/About'
 import Puppies from '../routes/Puppies'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
@@ -12,9 +12,11 @@ function App() {
     <div className="container">
       <Header />
       <div className="col-xs-8">
-        <Route exact path="/" render={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/puppies" component={Puppies}/>
+        <Switch>        
+          <Route path="/about" component={About}/>
+          <Route path="/puppies" component={Puppies}/>
+          <Route path="/" render={Home}/>
+        </Switch>
       </div>
     </div>
     </Router>
